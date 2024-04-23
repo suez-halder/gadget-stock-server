8ece70ed4a96539955fd9991f35c8eb622bc49f5
suezhalder <szhldr@gmail.com>
2024-04-23 19:52:35 +0600

    added features

--- src/app/modules/Gadget/gadget.constant.ts
+++ src/app/modules/Gadget/gadget.constant.ts
@@ -1,8 +1,13 @@
 import {
+  TCameraResolution,
   TCategory,
   TConnectivity,
+  TDisplayResolution,
+  TDisplaySize,
+  TFeatures,
   TOperatingSystem,
   TPowerSource,
+  TStorageCapacity,
 } from './gadget.interface'
 
 export const Category: TCategory[] = [
@@ -11,8 +16,35 @@ export const Category: TCategory[] = [
   'Headphone',
   'Smartwatch',
   'Speaker',
+  'Camera',
 ]
 
 export const OperatingSystem: TOperatingSystem[] = ['iOS', 'Android', 'Windows']
 export const Connectivity: TConnectivity[] = ['Bluetooth', 'Wi-Fi', ' USB-C']
 export const PowerSource: TPowerSource[] = ['battery-powered', 'plug-in']
+
+export const CameraResolution: TCameraResolution[] = [
+  '5mp',
+  '16mp',
+  '48mp',
+  '108mp',
+]
+export const StorageCapacity: TStorageCapacity[] = [
+  '256gb',
+  '512gb',
+  '1tb',
+  '2tb',
+]
+export const DisplaySize: TDisplaySize[] = [
+  '6inches',
+  '6.7inches',
+  '13inches',
+  '14inches',
+  '16inches',
+]
+export const DisplayResolution: TDisplayResolution[] = [
+  '720p',
+  '1280P',
+  '2k',
+  '4k',
+]
--- src/app/modules/Gadget/gadget.interface.ts
+++ src/app/modules/Gadget/gadget.interface.ts
@@ -4,10 +4,28 @@ export type TCategory =
   | 'Headphone'
   | 'Smartwatch'
   | 'Speaker'
+  | 'Camera'
 
 export type TOperatingSystem = 'iOS' | 'Android' | 'Windows'
 export type TConnectivity = 'Bluetooth' | 'Wi-Fi' | ' USB-C'
 export type TPowerSource = 'battery-powered' | 'plug-in'
+
+export type TCameraResolution = '5mp' | '16mp' | '48mp' | '108mp'
+export type TStorageCapacity = '256gb' | '512gb' | '1tb' | '2tb'
+export type TDisplaySize =
+  | '6inches'
+  | '6.7inches'
+  | '13inches'
+  | '14inches'
+  | '16inches'
+export type TDisplayResolution = '720p' | '1280P' | '2k' | '4k'
+
+export type TFeatures = {
+  cameraResolution: TCameraResolution
+  storageCapacity: TStorageCapacity
+  displaySize: TDisplaySize
+  displayResolution: TDisplayResolution
+}
 
 export type TGadget = {
   price: number
@@ -17,4 +35,5 @@ export type TGadget = {
   operatingSystem?: TOperatingSystem
   connectivity: TConnectivity
   powerSource: TPowerSource
+  features?: TFeatures
 }
